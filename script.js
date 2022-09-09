ad672d16cca6f7ea574a8ff62e03cbd4b4aea963
Tina Bigović <bigovict@gmail.com>
2022-09-09 20:18:56 +0200

    File upload

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+function displayMessage(message) {
+  document.querySelector(".message").textContent = message;
+}
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage("❌ No number! ");
+
+    // when player wins
+  } else if (guess === number) {
+    displayMessage("🎉 Correct number! ");
+    document.querySelector(".number").textContent = number;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+    // when the guess is different from the secret number
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess > number ? "😏 Too high! " : "😓 Too low! ");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
